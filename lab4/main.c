2570a873c3aabcfdf5656ee3d3f7a067e53d1eb5
Kamil Delekta <dkamil17@wp.pl>
2021-04-18 23:49:50 +0200

    Updating lab4/main.c

--- lab4/main.c
+++ lab4/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-//#include <gsl/gsl_blas.h>
+#include <gsl/gsl_blas.h>
 
 void naive_multiply(int N, int **mat1, int **mat2, int **res)
 {
@@ -33,10 +33,12 @@ void fillArrayRandom(int N, int **arr){
 }
 
 int main() {
+    srand(0);
     double naiveTimes[10] = {0};
     double betterTimes[10] = {0};
     double blasTimes[10] = {0};
-    for(int size = 150; size <= 1500; size += 150){
+    int startSize = 100;
+    for(int size = startSize; size <= startSize * 10; size += startSize){
         int **res = calloc(size, sizeof(int*));
         int **mat1 = calloc(size, sizeof(int*));
         int **mat2 = calloc(size, sizeof(int*));
@@ -48,18 +50,38 @@ int main() {
         fillArrayRandom(size, mat1);
         fillArrayRandom(size, mat2);
 //        printf("%d \n", mat1[0][1]);
+
         clock_t naive_start = clock();
         naive_multiply(size, mat1, mat2, res);
         clock_t naive_end = clock();
-        naiveTimes[size/150] = ((double)(naive_end - naive_start)) / CLOCKS_PER_SEC;;
+        naiveTimes[size/startSize] = ((double)(naive_end - naive_start)) / CLOCKS_PER_SEC;;
 
         clock_t better_start = clock();
         better_multiply(size, mat1, mat2, res);
         clock_t better_end = clock();
-        betterTimes[size/150] = ((double)(better_end - better_start)) / CLOCKS_PER_SEC;
+        betterTimes[size/startSize] = ((double)(better_end - better_start)) / CLOCKS_PER_SEC;
+
+        double *a = calloc(size*size, sizeof(double));
+        double *b = calloc(size*size, sizeof(double));
+        double *c = calloc(size*size, sizeof(double));
+
+        for(int i=0; i < size*size; i++){
+            a[i] = rand() % 100;
+            b[i] = rand() % 100;
+        }
+        gsl_matrix_view A = gsl_matrix_view_array(a, size, size);
+        gsl_matrix_view B = gsl_matrix_view_array(b, size, size);
+        gsl_matrix_view C = gsl_matrix_view_array(c, size, size);
 
         clock_t blas_start = clock();
+        gsl_blas_dgemm (CblasNoTrans, CblasNoTrans,
+                        1.0, &A.matrix, &B.matrix,
+                        0.0, &C.matrix);
         clock_t blas_end = clock();
+        blasTimes[size/startSize] = ((double)(blas_end - blas_start)) / CLOCKS_PER_SEC;
+        free(a);
+        free(b);
+        free(c);
         for(int i = 0; i < size; i++) {
             free(res[i]);
             free(mat1[i]);
@@ -69,36 +91,18 @@ int main() {
         free(mat1);
         free(mat2);
     }
+    printf("[");
     for(int i = 0; i < 10; i++){
-        printf("Naive %d: %f \n", i, naiveTimes[i]);
-        printf("Better %d: %f \n", i, betterTimes[i]);
+        printf("%f, ", naiveTimes[i]);
     }
-
-
-
-//    double a[] = { 0.11, 0.12, 0.13,
-//                   0.21, 0.22, 0.23 };
-//
-//    double b[] = { 1011, 1012,
-//                   1021, 1022,
-//                   1031, 1032 };
-//
-//    double c[] = { 0.00, 0.00,
-//                   0.00, 0.00 };
-//
-//    gsl_matrix_view A = gsl_matrix_view_array(a, 2, 3);
-//    gsl_matrix_view B = gsl_matrix_view_array(b, 3, 2);
-//    gsl_matrix_view C = gsl_matrix_view_array(c, 2, 2);
-//
-//    /* Compute C = A B */
-//
-//    gsl_blas_dgemm (CblasNoTrans, CblasNoTrans,
-//                    1.0, &A.matrix, &B.matrix,
-//                    0.0, &C.matrix);
-//
-//    printf ("[ %g, %g\n", c[0], c[1]);
-//    printf ("  %g, %g ]\n", c[2], c[3]);
-
-
+    printf("]\n[");
+    for(int i = 0; i < 10; i++){
+        printf("%f, ", betterTimes[i]);
+    }
+    printf("]\n[");
+    for(int i = 0; i < 10; i++){
+        printf("%f, ", blasTimes[i]);
+    }
+    printf("]");
     return 0;
 }
